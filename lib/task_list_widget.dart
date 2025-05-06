0abca674295052ad7b7e0e5b890fe24de029314c
net-shutou <shutou.ns@gmail.com>
2025-05-06 15:04:01 +0900

    https://github.com/net-shutou/tasklist/issues/15#issuecomment-2853372088

--- lib/task_list_widget.dart
+++ lib/task_list_widget.dart
@@ -16,25 +16,48 @@ class _TaskListWidgetState extends State<TaskListWidget> {
   final TextEditingController _addController = TextEditingController();
   final TextEditingController _editController = TextEditingController();
   int? _editingIndex;
-  
+
+  TaskListController get _controller => 
+      Provider.of<TaskListController>(context, listen: false);
+
   @override
   void dispose() {
     _addController.dispose();
-    _editController.removeListener(_handleEditControllerChange);
     _editController.dispose();
     super.dispose();
   }
 
-  void _handleEditControllerChange() {
-    if (_editingIndex != null) {
-      final newTitle = _editController.text;
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (mounted && _editingIndex != null && newTitle.isNotEmpty) {
-          final controller = Provider.of<TaskListController>(context, listen: false);
-          controller.updateTask(_editingIndex!, newTitle);
-        }
-      });
+  void _startEdit(int index, List tasks) {
+    setState(() {
+      _editingIndex = index;
+      _editController.text = tasks[index]['title'];
+    });
+  }
+
+  void _updateTask(int index, String newTitle) {
+    _controller.updateTask(index, newTitle);
+    setState(() => _editingIndex = null);
+  }
+
+  void _toggleCompletion(int index) {
+    _controller.toggleTaskCompletion(index);
+  }
+
+  void _deleteTask(int index) {
+    _controller.deleteTask(index);
+    if (_editingIndex == index) {
+      setState(() => _editingIndex = null);
     }
+  }
+
+  void _addTask() {
+    if (_addController.text.isEmpty) return;
+    _controller.addTask(_addController.text);
+    _addController.clear();
+  }
+
+  void _reorderTasks(int oldIndex, int newIndex) {
+    _controller.reorderTasks(oldIndex, newIndex);
   }
 
   @override
@@ -42,58 +65,28 @@ class _TaskListWidgetState extends State<TaskListWidget> {
     final tasks = Provider.of<TaskListController>(context).tasks;
 
     return Scaffold(
-      appBar: AppBar(title: Text('Task List')),
+      appBar: AppBar(title: const Text('Task List')),
       body: Column(
         children: [
           Expanded(
             child: TaskListView(
               tasks: tasks,
-              onReorder: (oldIndex, newIndex) {
-                final controller = Provider.of<TaskListController>(context, listen: false);
-                controller.reorderTasks(oldIndex, newIndex);
-              },
+              onReorder: _reorderTasks,
               buildTaskItem: (tasks, index) => buildTaskItem(
                 tasks: tasks,
                 index: index,
                 editController: _editController,
                 editingIndex: _editingIndex,
-                onEdit: (index) {
-                  setState(() {
-                    _editingIndex = index;
-                    _editController.text = tasks[index]['title'];
-                  });
-                },
-                onUpdateTask: (index, newTitle) {
-                  final controller = Provider.of<TaskListController>(context, listen: false);
-                  controller.updateTask(index, newTitle);
-                  setState(() {
-                    _editingIndex = null;
-                  });
-                },
-                onToggleCompletion: (index) {
-                  final controller = Provider.of<TaskListController>(context, listen: false);
-                  controller.toggleTaskCompletion(index);
-                },
-                onDeleteTask: (index) {
-                  final controller = Provider.of<TaskListController>(context, listen: false);
-                  controller.deleteTask(index);
-                  setState(() {
-                    if (_editingIndex == index) {
-                      _editingIndex = null;
-                    }
-                  });
-                },
+                onEdit: (index) => _startEdit(index, tasks),
+                onUpdateTask: _updateTask,
+                onToggleCompletion: _toggleCompletion,
+                onDeleteTask: _deleteTask,
               ),
             ),
           ),
           TaskInput(
             controller: _addController,
-            onAddTask: () {
-              if (_addController.text.isEmpty) return;
-              final controller = Provider.of<TaskListController>(context, listen: false);
-              controller.addTask(_addController.text);
-              _addController.clear();
-            },
+            onAddTask: _addTask,
           ),
         ],
       ),
